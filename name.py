d76484c40d7744657c5037811afdf8e17acf966f
Ja'Nayiah Jackson <53012610+Nayiah@users.noreply.github.com>
2025-11-30 19:50:43 -0500

    Add files via upload

---
+++ name.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+color('purple')
+
+# font, font size, font weight
+style = ('Times New Roman', 90, 'normal')
+
+# the string to write, how to write it, where to place the name
+write('Janayiah', font=style, align='center')
+hideturtle()
+exitonclick()
